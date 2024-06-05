20713eb14262ead6cf60d282636626c0fc5b991b
Rodrigo Teodoro de Freitas Figueira <rodrigo.figueira@hammerconsult.com.br>
2024-06-05 17:29:39 -0300

    :sparkles: feat: Create tables with Fluent Migrator

---
+++ src/FinancialManagement.Infra.Data/Migrations/CreateTableCategory.cs
@@ -0,0 +1,38 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagement.Infra.Data.Migrations
+{
+    [Migration(1)]
+    public class CreateTableCategory : Migration
+    {
+        readonly string TABLE_NAME = "categories";
+
+        public override void Down()
+        {
+            Delete.Table(TABLE_NAME);
+        }
+
+        public override void Up()
+        {
+            Create.Table(TABLE_NAME)
+                  .WithColumn("Id")
+                  .AsInt64()
+                  .PrimaryKey()
+                  .Identity()
+
+                  .WithColumn("Name")
+                  .AsString()
+                  .Unique()
+                  .NotNullable()
+
+                  .WithColumn("CreatedAt")
+                  .AsDateTime()
+                  .WithDefaultValue(DateTime.UtcNow);
+        }
+    }
+}
