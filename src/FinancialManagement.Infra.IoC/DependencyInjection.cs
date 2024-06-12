b64305e292d20725d8cdcdce8e77101657012e2a
Rodrigo Teodoro de Freitas Figueira <rodrigo.figueira@hammerconsult.com.br>
2024-06-12 17:28:59 -0300

    :tada: init: add new project IoC to the solution

---
+++ src/FinancialManagement.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using FinancialManagement.Domain.Interfaces;
+using FinancialManagement.Infra.Data;
+
+namespace FinancialManagement.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services
+                                                           ,IConfiguration configuration)
+        {
+            services.AddScoped<SqlConnection>(provider =>
+            {
+                var connectionString = configuration.GetConnectionString("SQLServer");
+                return new SqlConnection(connectionString);
+            });
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+            return services;
+        }
+    }
+}
