30cdf064c649939842841d68f8477be30226b1ff
Rodrigo Teodoro de Freitas Figueira <rodrigo.figueira@hammerconsult.com.br>
2024-06-13 14:38:57 -0300

    :sparkles: feat: Add CategoryDTO and Mapping

---
+++ src/FinancialManagement.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,4 @@
+﻿namespace FinancialManagement.Application.DTOs
+{
+    public record CategoryDTO(int Id, string Name);
+}
---
+++ src/FinancialManagement.Application/Mappings/CategoryMapping.cs
@@ -0,0 +1,18 @@
+﻿using FinancialManagement.Application.DTOs;
+using FinancialManagement.Domain.Entities;
+
+namespace FinancialManagement.Application.Mappings
+{
+    public static class CategoryMapping
+    {
+        public static Category ToEntity(this CategoryDTO categoryDTO)
+        {
+            return new(categoryDTO.Id, categoryDTO.Name);
+        }
+
+        public static CategoryDTO ToDTO(this Category category)
+        {
+            return new(category.Id, category.Name!);
+        }
+    }
+}
