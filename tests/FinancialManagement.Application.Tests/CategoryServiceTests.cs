6262e91cc816a2ddec789a2f0c481b8fd8c4a146
Rodrigo Teodoro de Freitas Figueira <rodrigo.figueira@hammerconsult.com.br>
2024-06-13 20:23:31 -0300

    :test_tube: test: Test for CategoryService.CreateAsync

---
+++ tests/FinancialManagement.Application.Tests/CategoryServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using FinancialManagement.Application.DTOs;
+using FinancialManagement.Application.Services;
+using FinancialManagement.Domain.Entities;
+using FinancialManagement.Domain.Interfaces;
+using FinancialManagement.Domain.Util;
+
+namespace FinancialManagement.Application.Tests
+{
+    public class CategoryServiceTests
+    {
+        [Fact]
+        public async Task CreateAsync_WithValidValues_ReturnDTO()
+        {
+            //arrange
+            Mock<ICategoryRepository> _repository = new();
+            _repository.Setup(r => r.CreateAsync(It.IsAny<Category>()))
+                                    .ReturnsAsync((Category category) 
+                                                    => Result<Category>.Success(new(1, category.Name!)));
+
+            CategoryService categoryService = new(_repository.Object);
+            CategoryPostDTO categoryPostDTO = new("Mock");
+
+            //act
+            var result = await categoryService.CreateAsync(categoryPostDTO);
+
+            //assert
+            Assert.True(result.IsSuccess);
+            Assert.False(result.IsFailure);
+            Assert.NotNull(result.Value);
+            Assert.Equal(categoryPostDTO.Name, result.Value.Name);
+            Assert.Equal(1, result.Value.Id);
+        }
+
+    }
+}
