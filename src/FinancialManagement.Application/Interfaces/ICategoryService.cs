26091b414ea757004bec3b364d316d59c327b836
Rodrigo Teodoro de Freitas Figueira <rodrigo.figueira@hammerconsult.com.br>
2024-06-13 20:22:36 -0300

    :sparkles: feat: Add ICategoryService and CategoryService

---
+++ src/FinancialManagement.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using FinancialManagement.Application.DTOs;
+using FinancialManagement.Domain.Util;
+
+namespace FinancialManagement.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<Result<IEnumerable<CategoryDTO>>> GetCategoriesAsync();
+        Task<Result<CategoryDTO>> GetByIdAsync(int id);
+        Task<Result<CategoryDTO>> CreateAsync(CategoryPostDTO categoryDTO);
+        Task<bool> UpdateAsync(CategoryDTO categoryDTO);
+        Task<bool> RemoveAsync(int id);
+    }
+}
