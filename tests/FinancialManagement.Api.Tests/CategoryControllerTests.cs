f21e7736becae478321231b1c9bcd60367e54da3
Rodrigo Teodoro de Freitas Figueira <rodrigo.figueira@hammerconsult.com.br>
2024-06-24 15:46:42 -0300

    :test_tube: test: Test happy path CategoryCreateAsync

---
+++ tests/FinancialManagement.Api.Tests/CategoryControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using FinancialManagement.Api.Controllers;
+using FinancialManagement.Application.DTOs;
+using FinancialManagement.Application.Interfaces;
+using FinancialManagement.Domain.Util;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace FinancialManagement.Api.Tests
+{
+    public class CategoryControllerTests
+    {
+        [Fact]
+        public async Task Create_WithValidParameters_ReturnOk()
+        {
+            //arrange
+            Mock<ICategoryService> categoryServiceMock = new();
+            categoryServiceMock.Setup(c => c.CreateAsync(It.IsAny<CategoryPostDTO>()))
+                .ReturnsAsync((CategoryPostDTO categoryPostDTO) => 
+                    Result<CategoryDTO>.Success(new CategoryDTO(1, categoryPostDTO.Name)));
+
+            Mock<IValidator<CategoryPostDTO>> validatorMock = new();
+            validatorMock.Setup(v => v.Validate(It.IsAny<CategoryPostDTO>())).Returns(new FluentValidation.Results.ValidationResult());
+
+            CategoryController categoryController = new(categoryServiceMock.Object);
+            CategoryPostDTO categoryPostDTO = new("Test");
+
+            //act
+            var result = await categoryController.Create(categoryPostDTO, validatorMock.Object);
+
+            //assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<CategoryDTO>(okResult.Value);
+            Assert.Equal(200, okResult.StatusCode);            
+            Assert.NotNull(returnValue);
+            Assert.Equal(1, returnValue.Id);
+            Assert.Equal("Test", returnValue.Name);
+        }
+
+
+    }
+}
