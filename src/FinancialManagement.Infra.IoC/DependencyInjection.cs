f91e2c764c93c8f031084ffa7424ce6b8282ade1
Rodrigo Figueira <rodrigoservice@gmail.com>
2024-10-18 12:08:26 -0300

    :recycle: Refactor

--- src/FinancialManagement.Infra.IoC/DependencyInjection.cs
+++ src/FinancialManagement.Infra.IoC/DependencyInjection.cs
@@ -1,36 +1,20 @@
-﻿using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using FinancialManagement.Domain.Interfaces;
-using FinancialManagement.Infra.Data;
-using FinancialManagement.Application.Interfaces;
-using FinancialManagement.Application.Services;
-using FluentValidation;
-using FinancialManagement.Application.Validators;
+﻿namespace FinancialManagement.Infra.IoC;
 
-namespace FinancialManagement.Infra.IoC
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services
-                                                           ,IConfiguration configuration)
+        _ = services.AddScoped(provider =>
         {
-            services.AddScoped<SqlConnection>(provider =>
-            {
-                var connectionString = configuration.GetConnectionString("SQLServer");
-                return new SqlConnection(connectionString);
-            });
-            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            var connectionString = configuration.GetConnectionString("SQLServer");
+            return new SqlConnection(connectionString);
+        });
+        _ = services.AddScoped<ICategoryRepository, CategoryRepository>();
+        _ = services.AddScoped<ICategoryService, CategoryService>();
 
-            services.AddScoped<ICategoryService, CategoryService>();
-
-            return services;
-        }
-
-        public static IServiceCollection AddFluentValidator(this IServiceCollection services)
-        {
-            services.AddValidatorsFromAssemblyContaining<CreateCategoryValidator>();
-            return services;
-        }
+        return services;
     }
+
+    public static IServiceCollection AddFluentValidator(this IServiceCollection services)
+        => services.AddValidatorsFromAssemblyContaining<CreateCategoryValidator>();
 }
