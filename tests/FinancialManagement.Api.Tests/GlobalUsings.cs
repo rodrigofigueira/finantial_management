9477a8802492e41dab3efa8d7369976830d1d533
Rodrigo Figueira <rodrigoservice@gmail.com>
2024-10-18 11:01:45 -0300

    :broom: Added global usings and file-scoped namespaces

---
+++ tests/FinancialManagement.Api.Tests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using FinancialManagement.Api.Controllers;
+global using FinancialManagement.Application.DTOs;
+global using FinancialManagement.Application.Interfaces;
+global using FinancialManagement.Domain.Util;
+global using FluentValidation;
+global using FluentValidation.Results;
+global using Microsoft.AspNetCore.Mvc;
+global using Moq;
